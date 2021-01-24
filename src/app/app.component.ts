9e35a4503f288e65ff2142e875acf32fb755094f
Nataliia Podpriatova <natalia.podpryatova@gmail.com>
2021-01-24 15:46:09 +0200

    passing content to a dynamic template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,57 @@
-import {AfterViewInit, Component, ComponentFactoryResolver, ComponentRef, ViewChild, ViewContainerRef} from '@angular/core';
-import {AuthFormComponent} from './auth-form/auth-form.component';
-import {User} from './auth-form/user';
+import {
+  AfterContentInit,
+  AfterViewInit,
+  Component,
+  ComponentFactoryResolver,
+  ComponentRef,
+  TemplateRef,
+  ViewChild,
+  ViewContainerRef
+} from '@angular/core';
+import { AuthFormComponent } from './auth-form/auth-form.component';
+import { User } from './auth-form/user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent implements AfterViewInit, AfterContentInit {
   component: ComponentRef<AuthFormComponent>;
-  @ViewChild('entry', { read: ViewContainerRef }) entry: ViewContainerRef;
+  @ViewChild('tmpl') template: TemplateRef<any>;
+  @ViewChild('entry', {read: ViewContainerRef}) entry: ViewContainerRef;
 
-  constructor(
-    private resolver: ComponentFactoryResolver
-  ) {}
+  constructor( private resolver: ComponentFactoryResolver ) {}
 
-  ngAfterViewInit(): void {
+  ngAfterContentInit(): void {
     setTimeout(() => {
       const authFormFactory = this.resolver.resolveComponentFactory(AuthFormComponent);
-      this.component = this.entry.createComponent(authFormFactory);
+      this.entry.createComponent(authFormFactory);
+      this.component = this.entry.createComponent(authFormFactory, 0);
       this.component.instance.title = 'Create user';
       this.component.instance.submitted.subscribe(this.loginUser);
     });
   }
 
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+    this.entry.createEmbeddedView(this.template, {
+      $implicit: 'Nata Gildry',
+      location: 'Ukraine, UA'
+    });
+  });
+  }
+
   loginUser(user: User): void {
     console.log('Login', user);
   }
+
   destroyComponent(): void {
     this.component.destroy();
+  }
+
+  moveComponent(): void {
+    this.entry.move(this.component.hostView, 1);
   }
 
 }

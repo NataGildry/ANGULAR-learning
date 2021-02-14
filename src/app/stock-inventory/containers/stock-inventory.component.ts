28d48f9d7e64c41742984c869ce58e4303e48ba5
Nataliia Podpriatova <natalia.podpryatova@gmail.com>
2021-02-15 00:25:40 +0200

    added componentizing FormGroups

--- src/app/stock-inventory/containers/stock-inventory.component.ts
+++ src/app/stock-inventory/containers/stock-inventory.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import {FormArray, FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-stock-inventory',
@@ -10,9 +10,14 @@ export class StockInventoryComponent  {
 
   form = new FormGroup({
     store: new FormGroup({
-      branch: new FormControl('B182'),
-      code: new FormControl('1234')
-    })
+      branch: new FormControl(''),
+      code: new FormControl('')
+    }),
+    selector: new FormGroup({
+      product_id: new FormControl(''),
+      quantity: new FormControl(10)
+    }),
+    stock: new FormArray([])
   })
 
   onSubmit(): void {

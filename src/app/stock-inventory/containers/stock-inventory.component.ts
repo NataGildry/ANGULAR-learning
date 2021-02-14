0e013edccdfefdc1688a5cf9d48ba13142ff49db
Nataliia Podpriatova <natalia.podpryatova@gmail.com>
2021-02-14 23:49:43 +0200

    added FormControls and FormGroups

--- src/app/stock-inventory/containers/stock-inventory.component.ts
+++ src/app/stock-inventory/containers/stock-inventory.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-stock-inventory',
   templateUrl: './stock-inventory.component.html',
   styleUrls: ['./stock-inventory.component.css']
 })
-export class StockInventoryComponent implements OnInit {
+export class StockInventoryComponent  {
 
-  constructor() { }
+  form = new FormGroup({
+    store: new FormGroup({
+      branch: new FormControl('B182'),
+      code: new FormControl('1234')
+    })
+  })
 
-  ngOnInit(): void {
+  onSubmit(): void {
+    console.log('Submit:', this.form.value);
   }
 
 }
